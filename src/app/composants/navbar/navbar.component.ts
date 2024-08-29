3403e64c05e28bd79f872d8c0a40977ba3cdd8c7
Michée Kolony <micheekolony71@gmail.com>
2024-08-29 14:25:33 +0100

    add mobile menu

--- src/app/composants/navbar/navbar.component.ts
+++ src/app/composants/navbar/navbar.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+menu: boolean = true;
 
+togglemenu(){
+  this.menu = false
+}
+closemenu(){
+  this.menu = true;
+}
 }
