c8525be5509aec4f0e6f8f08cd585fb4f90f67cb
Michée Kolony <micheekolony71@gmail.com>
2024-08-30 15:43:36 +0100

    ajout FAQ

--- src/app/composants/contact/contact.component.ts
+++ src/app/composants/contact/contact.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  splash : boolean = true;
 
+  ngOnInit() : void {
+   setTimeout(() => {
+     this.splash = false;
+   }, 5000);
+  }
 }
