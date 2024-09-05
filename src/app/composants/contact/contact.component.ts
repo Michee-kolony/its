edd978cd56601c39e80e6ecf4fd0227d01e103f0
Michée Kolony <micheekolony71@gmail.com>
2024-09-05 11:38:15 +0100

    add logo

--- src/app/composants/contact/contact.component.ts
+++ src/app/composants/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-contact',
@@ -7,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
   splash : boolean = true;
-
+  mySwiper!: Swiper;
   ngOnInit() : void {
+    this.mySwiper = new Swiper('.swiper-container', {
+      // options Swiper ici
+      direction: 'horizontal',
+
+      loop: true,
+      autoplay: {
+        delay: 3000, // Temps en millisecondes entre chaque slide
+        disableOnInteraction: false, // Continuer après interaction de l'utilisateur
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints:{
+        0:{
+          slidesPerView : 3
+        },
+        620:{
+          slidesPerView : 4
+        },
+        1024:{
+          slidesPerView : 5
+        }
+  
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
    setTimeout(() => {
      this.splash = false;
    }, 5000);
