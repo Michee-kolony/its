dd2cb881045c8e663c2b0d9ca7034c5888d73968
Michée Kolony <micheekolony71@gmail.com>
2024-09-06 23:35:59 +0100

    optimisation image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './composants/home/home.component';
 import { AboutComponent } from './composants/about/about.component';
 import { ServicesComponent } from './composants/services/services.component';
 import { ContactComponent } from './composants/contact/contact.component';
+import { FaqComponent } from './composants/faq/faq.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path:'home', component: HomeComponent},
   {path:'about', component:AboutComponent},
   {path:'services', component:ServicesComponent},
-  {path:'contact', component:ContactComponent}
+  {path:'contact', component:ContactComponent},
+  {path:'faq', component:FaqComponent}
 ];
 
 @NgModule({
